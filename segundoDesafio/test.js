8ff693bc14bf02f174b8a95792d5ba06f862aa61
Mario Massonnat <93622442+MarucoMass@users.noreply.github.com>
2022-06-10 03:27:03 -0300

    segundoDesafio

---
+++ segundoDesafio/test.js
@@ -0,0 +1,25 @@
+const Contenedor = require('./segundo-desafio.js')
+
+const producto = new Contenedor('./productos.txt')
+
+const test = async () => {
+    // const testOne = await producto.getAll()
+    // console.log(testOne);
+
+    // const testTwo = await producto.save({
+    //     title: 'heladera',
+    //     price: 105000,
+    //     thumbnail: 'https://yuhmak.vteximg.com.br/arquivos/ids/157106/E0000013680-HELADERA-BRIKET-1810-BL-INVERTER-356L--1-.png?v=637421644884930000',
+    // })
+    // console.log(testTwo);
+
+    // const testThree = await producto.getById(2)
+    // console.log(testThree);
+
+    // const testFour = await producto.deleteById(3)
+    // console.log(testFour);
+
+    // await producto.deleteAll()
+}
+
+test()
